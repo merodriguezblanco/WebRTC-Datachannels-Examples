7b0bdf716cffc24322c4a6d3a98e33c0c5fe9a44
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-06-21 12:28:51 -0300

    Display video camera stream in HTML5 video tag.

---
+++ get_user_media/use_video_camera.js
@@ -0,0 +1,22 @@
+// Compatibility between browsers.
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var app = {};
+
+app.constraints = { video: true, audio: false };
+
+app.successCallback = function(stream) {
+  console.log("Success callback.");
+  video = document.getElementById("video_display");
+  video.src = window.URL.createObjectURL(stream);
+  video.play();
+};
+
+app.errorCallback = function(error) {
+  console.log("Error callback, error: " + error);
+};
+
+(function() {
+  console.log("Calling navigator.getUserMedia.");
+  navigator.getUserMedia(app.constraints, app.successCallback, app.errorCallback);
+})();
